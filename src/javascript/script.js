5d6e2dbd1c1d9c5ba114fc8aac00fd0d4efd3fb4
Miguel Fernandes Silva <miguel.0fersilva0@gmail.com>
2026-09-07 00:16:58 -0300

    Uploading uncrashed archives versions

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
 const ticketBtn = document.getElementById('btnTickets');
 
 ticketBtn.addEventListener('click', () => window.open('../../src/pages/ingresso.html', '_self'));
-=======
-const ticketBtn = document.getElementById('btnTickets');
-
-ticketBtn.addEventListener('click', () => window.open('../pages/ingresso.html', '_self'));
->>>>>>> b5c2edff873a3486cc3d0ae4793907e1646d4e70
